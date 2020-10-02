e972ff7614646b25db717d845d4ff7c82da2770c
Aurelien RICHE <a.riche@betclicgroup.com>
2020-10-02 17:07:49 +0200

    Add Redux + paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ToDoComponent } from './to-do/to-do.component';
+import { StoreModule } from '@ngrx/store';
+import { ToDoReducer } from '@reducers/todo.reducer';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ToDoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ToDoComponent],
+  imports: [BrowserModule, FormsModule, StoreModule.forRoot({ todos: ToDoReducer })],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as ToDoActions from '@actions/todo.action';
+import ToDo from '@models/todo.model';
+import ToDoState from '@states/todo.state';
 
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.component.html',
-  styleUrls: ['./to-do.component.css']
+  styleUrls: ['./to-do.component.css'],
 })
-export class ToDoComponent implements OnInit {
+export class ToDoComponent implements OnInit, OnDestroy {
+  public todo$: Observable<ToDoState>;
+  public ToDoSubscription: Subscription;
+  public ToDoList: ToDo[] = [];
 
-  constructor() { }
+  public Title = '';
+  public IsCompleted = false;
 
-  ngOnInit(): void {
+  public todoError: Error = null;
+
+  constructor(private store: Store<{ todos: ToDoState }>) {
+    this.todo$ = store.pipe(select('todos'));
   }
 
+  ngOnInit(): void {
+    this.ToDoSubscription = this.todo$
+      .pipe(
+        map((x) => {
+          this.ToDoList = x.ToDos;
+          this.todoError = x.ToDoError;
+        })
+      )
+      .subscribe();
+
+    this.store.dispatch(ToDoActions.BeginGetToDoAction());
+  }
+
+  createToDo(): void {
+    const todo: ToDo = { Title: this.Title, IsCompleted: this.IsCompleted };
+    this.store.dispatch(ToDoActions.BeginCreateToDoAction({ payload: todo }));
+    this.Title = '';
+    this.IsCompleted = false;
+  }
+
+  ngOnDestroy(): void {
+    if (this.ToDoSubscription) {
+      this.ToDoSubscription.unsubscribe();
+    }
+  }
 }
